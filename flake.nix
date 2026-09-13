e89ffb68085ded547b3aed530e8453dab61695d9
Joakim Repomaa <joakim.repomaa@mocoapp.com>
2026-09-13 12:35:21 +0300

    migrate to hugo 0.165 and statichost.eu

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ] (system: fn nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          # Hugo (extended build: includes SCSS/libsass support)
+          hugo
+          # needed for theme submodule management
+          git
+        ];
+      };
+    });
+  };
+}
